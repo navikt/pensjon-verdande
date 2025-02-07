16312dfa039e16b77c25e10ed39c18631afc7611
Emil Kalstø <emil.kalsto@nav.no>
2025-02-07 13:17:48 +0100

    Låste vedtak: Lås opp

---
+++ app/routes/laaste-vedtak.laasOpp.tsx
@@ -0,0 +1,39 @@
+import type { ActionFunctionArgs } from '@remix-run/node'
+
+
+import { requireAccessToken } from '~/services/auth.server'
+import { env } from '~/services/env.server'
+
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+
+  const accessToken = await requireAccessToken(request)
+
+  const data = await request.json()
+  return await laasOpp(accessToken, data.vedtakId)
+}
+
+async function laasOpp(
+  accessToken: string,
+  vedtakId: string,
+) {
+
+  const response = await fetch(
+    `${env.penUrl}/api/behandling/laaste-vedtak/laas-opp/${vedtakId}`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+        'X-Request-ID': crypto.randomUUID(),
+      },
+    },
+  )
+
+  return {
+    success: response.ok,
+  }
+}
+
+export type LaasOppResultat = {
+  success: boolean
+}
