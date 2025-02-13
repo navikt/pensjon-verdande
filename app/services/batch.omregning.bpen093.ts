dbf84bc4b7abda72d170882c294e50fda2e9c0f7
Hans Magnus Hallaraker <hans.magnus.hallaraker@nav.no>
2025-02-13 12:01:30 +0100

    Legger til url, x-request-id i oppretting av omregning.

--- app/services/batch.omregning.bpen093.ts
+++ app/services/batch.omregning.bpen093.ts
@@ -1,15 +1,16 @@
 import { OmregningRequest, StartBatchResponse } from '~/types'
+import { env } from '~/services/env.server'
 
 export async function opprettOmregningbehandling(
   accessToken: string, payload: OmregningRequest): Promise<StartBatchResponse> {
-
-  const response = await fetch(`/api/saksbehandling/batch/omregning/bpen093`, {
+  const response = await fetch(`${env.penUrl}/api/saksbehandling/behandling/omregning/opprett`, {
     method: 'POST',
-    body: JSON.stringify(payload),
     headers: {
       Authorization: `Bearer ${accessToken}`,
       'Content-Type': 'application/json',
+      'X-Request-ID': crypto.randomUUID(),
     },
+    body: JSON.stringify(payload),
   })
 
   if (response.ok) {
