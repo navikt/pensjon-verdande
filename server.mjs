5051a1e95bc08a13e40e61c3202c23090b63dddf
Per Christian Moen <per.christian.moen@nav.no>
2025-03-12 12:29:53 +0100

    Tar i bruk pino for logging

--- server.mjs
+++ server.mjs
@@ -4,7 +4,11 @@ import express from 'express'
 // notice that the result of `remix build` is "just a module"
 import * as build from './build/index.js'
 
+const pino = require('pino-http')()
+const logger = require('pino')()
+
 const app = express()
+app.use(pino)
 app.use(express.static('public'))
 
 app.get(['/internal/live', '/internal/ready'], (_, res) => res.sendStatus(200))
@@ -13,5 +17,5 @@ app.get(['/internal/live', '/internal/ready'], (_, res) => res.sendStatus(200))
 app.all('*', createRequestHandler({ build }))
 
 app.listen(8080, () => {
-  console.log('App listening on http://localhost:8080')
+  logger.info('App listening on http://localhost:8080')
 })
