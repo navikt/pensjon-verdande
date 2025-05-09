ae7ffdfe6d6f7f6567d531bf46e19b162c6c20ce
Emil Kalstø <emil.kalsto@nav.no>
2025-05-09 10:47:50 +0200

    Regulering: Avsluttende aktiviteter
    Co-authored-by: Anders Refsahl <anders.refsahl@nav.no>

---
+++ app/routes/batch.reguleringv2.avsluttendeaktiviteter.tsx
@@ -0,0 +1,98 @@
+import { ActionFunctionArgs, redirect } from '@remix-run/node'
+import { requireAccessToken } from '~/services/auth.server'
+import { Alert, Button, HStack, VStack } from '@navikt/ds-react'
+import { getReguleringDetaljer } from '~/routes/batch.reguleringv2'
+import React, { useState } from 'react'
+import { ConfirmationModal } from '~/routes/batch.reguleringv2.administrerbehandlinger'
+import { Simulate } from 'react-dom/test-utils'
+import submit = Simulate.submit
+import { useSubmit } from '@remix-run/react'
+import { env } from '~/services/env.server'
+import { useActionData } from 'react-router'
+
+
+export const loader = async ({ request }: ActionFunctionArgs) => {
+  return {}
+}
+
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+  const accessToken = await requireAccessToken(request)
+  const data = await request.json() as FormType
+
+
+  let urlPostfix = ''
+  switch (data.action) {
+    case 'avbrytBehandlingerFeiletMotPOPP':
+      urlPostfix = '/avbryt/oppdaterpopp'
+      break
+    case 'avbrytBehandlingerFeiletIBeregnYtelse':
+      urlPostfix = '/avbryt/beregnytelser'
+      break
+  }
+
+  const res = await fetch(
+    `${env.penUrl}/api/vedtak/regulering${urlPostfix}`,
+    {
+    method: 'POST',
+    body: JSON.stringify({}),
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+      'X-Request-ID': crypto.randomUUID(),
+    },
+  })
+
+
+  if(res.status !== 200) {
+    throw new Error(await res.text())
+  }
+
+  return {success: true, action: data.action}
+}
+
+
+export default function Avsluttendeaktiviteter() {
+  const [openConfirmationModal, setOpenConfirmationModal] = useState<ActionType | null>(null)
+  const submit = useSubmit();
+  const actionData = useActionData() as ActionData | undefined
+
+
+  function avbrytBehandlingerFeiletMotPopp() {
+    submit({action: "avbrytBehandlingerFeiletMotPOPP"}, {method: "post", encType: "application/json"})
+    setOpenConfirmationModal(null)
+  }
+
+  function avbrytBehandlerFeiletIBeregnYtelse() {
+    submit({action: "avbrytBehandlingerFeiletIBeregnYtelse"}, {method: "post", encType: "application/json"})
+    setOpenConfirmationModal(null)
+  }
+
+  return (
+    <VStack gap="5">
+      {actionData?.success && <Alert variant="success" inline>
+        Behandlinger avbrutt mot {actionData.action === "avbrytBehandlingerFeiletMotPOPP" ? "POPP" : "Beregn Ytelse"}
+      </Alert>}
+    <HStack gap="5">
+      <Button onClick={() => setOpenConfirmationModal("avbrytBehandlingerFeiletMotPOPP")}>
+        Avbryt behandlinger feilet mot POPP
+      </Button>
+      <Button onClick={() => setOpenConfirmationModal("avbrytBehandlingerFeiletIBeregnYtelse")}>
+        Avbryt behandlinger feilet i Beregn Ytelse
+      </Button>
+      <ConfirmationModal text="Er du sikker på at du vil avbryte behandlinger feilet mot POPP" showModal={openConfirmationModal === "avbrytBehandlingerFeiletMotPOPP"} onOk={() => {avbrytBehandlingerFeiletMotPopp()}} onCancel={() => setOpenConfirmationModal(null)} />
+      <ConfirmationModal text="Er du sikker på at du vil avbryte behandlinger feilet i Beregn Ytelse" showModal={openConfirmationModal === "avbrytBehandlingerFeiletIBeregnYtelse"} onOk={() => {avbrytBehandlerFeiletIBeregnYtelse()}} onCancel={() => setOpenConfirmationModal(null)} />
+    </HStack>
+    </VStack>
+  )
+}
+
+
+type FormType = {
+  action: ActionType
+}
+
+type ActionData = {
+  success: boolean,
+  action: ActionType
+}
+type ActionType = "avbrytBehandlingerFeiletMotPOPP" | "avbrytBehandlingerFeiletIBeregnYtelse" | null
