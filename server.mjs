70e35f04cef727c6883f60978557254011900fae
Per Christian Moen <per.christian.moen@nav.no>
2025-05-21 10:53:17 +0200

    Bytter ut ecsLogger med egen config som har en allowlist for headere

--- server.mjs
+++ server.mjs
@@ -2,12 +2,37 @@ import { createRequestHandler } from '@remix-run/express'
 import express from 'express'
 import PinoHttp from 'pino-http'
 import pino from 'pino'
-import { ecsFormat } from '@elastic/ecs-pino-format'
 
 // notice that the result of `remix build` is "just a module"
 import * as build from './build/index.js'
 
-const logger = pino(ecsFormat())
+const logger = pino({
+  serializers: {
+    req: pino.stdSerializers.wrapRequestSerializer((req) => {
+      return {
+        id: req.raw.id,
+        method: req.raw.method,
+        path: req.raw.url.split('?')[0], // Remove query params which might be sensitive
+        // Allowlist useful headers
+        headers: {
+          host: req.raw.headers.host,
+          'user-agent': req.raw.headers['user-agent'],
+          referer: req.raw.headers.referer,
+        }
+      };
+    }),
+    res: pino.stdSerializers.wrapResponseSerializer((res) => {
+      return {
+        statusCode: res.raw.statusCode,
+        // Allowlist useful headers
+        headers: {
+          'content-type': res.raw.headers['content-type'],
+          'content-length': res.raw.headers['content-length'],
+        }
+      };
+    }),
+  },
+})
 
 const app = express()
 app.use(PinoHttp({ logger: logger }))
