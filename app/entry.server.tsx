5e4e8613a112e193d8df58fe32f4ab893d304e95
Per Christian Moen <per.christian.moen@nav.no>
2025-06-08 13:37:34 +0200

    Rydder opp i isbot kode

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -47,22 +47,13 @@ export default function handleRequest(
       );
 }
 
-// We have some Remix apps in the wild already running with isbot@3 so we need
-// to maintain backwards compatibility even though we want new apps to use
-// isbot@4.  That way, we can ship this as a minor Semver update to @remix-run/dev.
 function isBotRequest(userAgent: string | null) {
   if (!userAgent) {
     return false;
   }
 
-  // isbot >= 3.8.0, >4
   if ("isbot" in isbotModule && typeof isbotModule.isbot === "function") {
     return isbotModule.isbot(userAgent);
-  }
-
-  // isbot < 3.8.0
-  if ("default" in isbotModule && typeof isbotModule.default === "function") {
-    return isbotModule.default(userAgent);
   }
 
   return false;
