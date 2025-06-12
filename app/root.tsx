1451bc5272dc2dcd2329e96649c5b125019fd0cf
Hans Magnus Hallaraker <hans.magnus.hallaraker@nav.no>
2025-06-12 12:34:06 +0200

    Oppdaterer sidemeny for omregning
    Co-authored-by: Ingleiv Johansen <ingleiv.halvard.johansen@nav.no>

--- app/root.tsx
+++ app/root.tsx
@@ -119,14 +119,7 @@ export default function App() {
                     <li>
                       <NavLink to={`/bpen091`}>Fastsette inntekt for uføretrygd</NavLink>
                     </li>
-                    <li>
-                      <NavLink to={`/omregning`}>Omregn ytelser</NavLink>
-                      <ul>
-                        <li>
-                          <NavLink to={`/omregningStatistikk`}>Omregn statistikk</NavLink>
-                        </li>
-                      </ul>
-                    </li>
+
                     <li>
                       <NavLink to={`/batch/regulering`}>Regulering</NavLink>
                     </li>
@@ -142,7 +135,17 @@ export default function App() {
                     <li>
                       <NavLink to={`/batch/inntektskontroll`}>Inntektskontroll</NavLink>
                     </li>
-
+                    <li>
+                      <Accordion.Item defaultOpen={false}>
+                        <Accordion.Header>
+                          Omregning
+                        </Accordion.Header>
+                        <Accordion.Content>
+                          <NavLink to={`/omregning`}>Omregn ytelser</NavLink>
+                          <NavLink to={`/omregningStatistikk`}>Omregn statistikk</NavLink>
+                        </Accordion.Content>
+                      </Accordion.Item>
+                    </li>
                   </ul>
                 </Accordion.Content>
               </Accordion.Item>
