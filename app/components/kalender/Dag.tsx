9bd09eb1d658d9ed31ef2ba4eb024fb70b7371ff
Per Christian Moen <per.christian.moen@nav.no>
2025-06-14 21:30:11 +0200

    Bruker grå tekst for behandlinger i andre måneder

--- app/components/kalender/Dag.tsx
+++ app/components/kalender/Dag.tsx
@@ -16,7 +16,7 @@ export type Props = {
 export default function Dag(props: Props) {
   let dagLabel: string
   if (props.dato.getDate() === 1) {
-    dagLabel = props.dato.getDate() + '. ' +  props.dato.toLocaleDateString('no-NO', { month: 'long' })
+    dagLabel = props.dato.getDate() + '. ' + props.dato.toLocaleDateString('no-NO', { month: 'long' })
   } else {
     dagLabel = props.dato.getDate().toString()
   }
@@ -30,17 +30,17 @@ export default function Dag(props: Props) {
 
   function dayRow() {
     if (props.dato.getDay() === 1) {
-      return(
-          <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-            <span style={{ textAlign: 'left', color: 'gray', fontSize: '0.9em' }}>{getWeek(props.dato)}</span>
-            <span style={{ textAlign: 'right', color: textColor }}>{dagLabel}</span>
-          </div>
-      );
+      return (
+        <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+          <span style={{ textAlign: 'left', color: 'gray', fontSize: '0.9em' }}>{getWeek(props.dato)}</span>
+          <span style={{ textAlign: 'right', color: textColor }}>{dagLabel}</span>
+        </div>
+      )
 
     } else {
-      return(
-        <div style={{color: textColor, textAlign: 'right'}}>{dagLabel}</div>
-      );
+      return (
+        <div style={{ color: textColor, textAlign: 'right' }}>{dagLabel}</div>
+      )
     }
   }
 
@@ -59,20 +59,24 @@ export default function Dag(props: Props) {
             }}
           >
             <Link as={ReactRouterLink}
-              to={`/behandling/${behandling.behandlingId}`}
-              variant="neutral"
+                  to={`/behandling/${behandling.behandlingId}`}
+                  variant="neutral"
                   underline={false}
+                  style={{ color: textColor }}
             >
               {decodeBehandling(behandling.type)}
             </Link>
           </span>
-          {props.visKlokkeSlett && <span style={{ textAlign: 'right', color: textColor }}>{new Date(behandling.opprettet).getHours().toString().padStart(2, '0') + ':' + new Date(behandling.opprettet).getMinutes().toString().padStart(2, '0')}</span>}
+          {props.visKlokkeSlett && <span style={{
+            textAlign: 'right',
+            color: textColor,
+          }}>{new Date(behandling.opprettet).getHours().toString().padStart(2, '0') + ':' + new Date(behandling.opprettet).getMinutes().toString().padStart(2, '0')}</span>}
         </div>
-      ));
+      ))
   }
 
-  return(
-    <table style={{width: '100%', tableLayout: 'fixed'}}>
+  return (
+    <table style={{ width: '100%', tableLayout: 'fixed' }}>
       <thead>
       <tr>
         {dayRow()}
@@ -82,6 +86,6 @@ export default function Dag(props: Props) {
       <tr>{dagensBehandlinger()}</tr>
       </tbody>
     </table>
-  );
+  )
 
 }
