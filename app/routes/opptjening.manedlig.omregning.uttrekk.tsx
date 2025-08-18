cc0bc3bd6dfd4100bc35310c04995f0cc9399752
Monika Sperstad Koller <monika.koller@nav.no>
2025-08-18 10:46:34 +0200

    Navneendring bpen055 -> Opptjeningsendring

--- app/routes/opptjening.manedlig.omregning.uttrekk.tsx
+++ app/routes/opptjening.manedlig.omregning.uttrekk.tsx
@@ -1,14 +1,14 @@
 import type { ActionFunctionArgs } from 'react-router';
 import { redirect } from 'react-router';
 import { requireAccessToken } from '~/services/auth.server'
-import { opprettBpen055MandeligUttrekk } from '~/services/batch.bpen055ManedligUttrekkserver'
+import { opprettOpptjeningsendringMandeligUttrekk } from '~/services/batch.opptjeningsendringManedligUttrekkserver'
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData()
   const updates = Object.fromEntries(formData)
   const accessToken = await requireAccessToken(request)
 
-  let response = await opprettBpen055MandeligUttrekk(accessToken, +updates.behandlingsmaned)
+  let response = await opprettOpptjeningsendringMandeligUttrekk(accessToken, +updates.behandlingsmaned)
 
   return redirect(`/behandling/${response.behandlingId}`)
 }
