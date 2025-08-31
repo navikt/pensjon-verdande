77540064f7a6fcf9f3c1958dcb145acb36e6c23e
Per Christian Moen <per.christian.moen@nav.no>
2025-08-31 13:12:02 +0200

    Legger til react-router-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from 'vite'
 import { reactRouter } from '@react-router/dev/vite';
+import { reactRouterDevTools } from "react-router-devtools"
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
+    reactRouterDevTools(),
     reactRouter(),
     tsconfigPaths(),
   ],
