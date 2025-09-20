2bf7346c324361d9dc38a10d785b1ae84f78abdd
Per Christian Moen <per.christian.moen@nav.no>
2025-09-20 15:23:40 +0200

    Bruker akselkomponenter

--- app/uforetrygd/bestem-etteroppgjor-resultat._index.tsx
+++ app/uforetrygd/bestem-etteroppgjor-resultat._index.tsx
@@ -1,28 +1,8 @@
-import { Select } from '@navikt/ds-react'
+import { Button, Heading, Select, VStack } from '@navikt/ds-react'
 import type { ActionFunctionArgs } from 'react-router'
-import { Form } from 'react-router';
+import { Form } from 'react-router'
 import { requireAccessToken } from '~/services/auth.server'
 import { startBestemEtteroppgjorResultat } from '~/uforetrygd/bestem-etteroppgjor-resultat.server'
-
-export default function BestemEtteroppgjorResultat() {
-  return <div>
-    <Select
-      label="DryRun"
-      size="small"
-      name="dryRun"
-      defaultValue="true"
-    >
-      <option value="true">Ja</option>
-      <option value="false">Nei</option>
-    </Select>
-
-    <Form method="post">
-    <button type="submit">
-      Opprett
-    </button>
-    </Form>
-  </div>
-}
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const accessToken = await requireAccessToken(request)
@@ -30,6 +10,28 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData()
   const updates = Object.fromEntries(formData)
 
-  const response = await startBestemEtteroppgjorResultat(accessToken, updates)
-  return null;
+  await startBestemEtteroppgjorResultat(accessToken, updates)
+}
+
+export default function BestemEtteroppgjorResultat() {
+  return <VStack gap="4">
+
+    <Heading size="small" level="1">Bestem etteroppgjørsresultat</Heading>
+    <Form method="post" style={{ width: '20em' }}>
+      <VStack gap="4">
+        <Select
+          label="DryRun"
+          size="small"
+          name="dryRun"
+          defaultValue="true"
+        >
+          <option value="true">Ja</option>
+          <option value="false">Nei</option>
+        </Select>
+        <Button type="submit" size="small">
+          Opprett
+        </Button>
+      </VStack>
+    </Form>
+  </VStack>
 }
