81b9afafa8bfbadb222acabf1811c6e392116e83
Morten Normann <morten.normann@nav.no>
2025-10-15 17:01:20 +0200

    Legg til knapp for å starte årlig opptjeningsendring

--- app/routes.ts
+++ app/routes.ts
@@ -167,9 +167,9 @@ export default [
     route('omregning/omregningsaker', 'omregning/omregning.omregningsaker.tsx'),
     route('omregningStatistikk', 'omregning/omregningStatistikk._index.tsx'),
 
-    route('opptjening/arlig/omregning', 'opptjening/opptjening.arlig.omregning._index.tsx'),
-    route('opptjening/manedlig/omregning', 'opptjening/opptjening.manedlig.omregning.tsx'),
-    route('/opptjening/manedlig/omregning/opprett', 'opptjening/opptjening.manedlig.omregning.opprett.tsx'),
+    route('opptjening/arlig/omregning', 'opptjening/arlig/opptjening.arlig.omregning.tsx'),
+    route('opptjening/manedlig/omregning', 'opptjening/manedlig/opptjening.manedlig.omregning.tsx'),
+    route('/opptjening/manedlig/omregning/opprett', 'opptjening/manedlig/opptjening.manedlig.omregning.opprett.tsx'),
 
     route('sok', 'sok/sok.tsx'),
   ]),
