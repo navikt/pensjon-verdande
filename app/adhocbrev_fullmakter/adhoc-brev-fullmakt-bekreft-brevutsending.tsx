4373f2fabb2c4142cfc9ff71a87e923425cc9aec
Emil Kalstø <emil.kalsto@nav.no>
2025-10-31 15:46:01 +0100

    Ekstra bekreftelse på at brev skal sendes ut

---
+++ app/adhocbrev_fullmakter/adhoc-brev-fullmakt-bekreft-brevutsending.tsx
@@ -0,0 +1,52 @@
+import { Box, Button, Heading, TextField, VStack } from '@navikt/ds-react'
+import { useState } from 'react'
+import { type ActionFunctionArgs, Form, redirect, useNavigation } from 'react-router'
+import { bekreftAdHocBrevSlettFullmaktBprofBrevUtsending } from '~/adhocbrev_fullmakter/adhoc-brev-fullmakt.server'
+
+import { requireAccessToken } from '~/services/auth.server'
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData()
+  const accessToken = await requireAccessToken(request)
+
+  const response = await bekreftAdHocBrevSlettFullmaktBprofBrevUtsending(
+    accessToken,
+    formData.get('behandlingId') as string,
+  )
+
+  return redirect(`/behandling/${response.behandlingId}`)
+}
+
+export default function AdhocBrevFullmaktBekreftBrevutsending() {
+  const navigation = useNavigation()
+
+  const isSubmitting = navigation.state === 'submitting'
+  const [behandlingId, setBehandlingId] = useState<string | ''>('')
+
+  return (
+    <VStack gap={'4'}>
+      <Box.New className={'aksel-pageblock--lg'}>
+        <Heading size={'medium'} level={'1'}>
+          Bekreft at ad-hoc brevbestilling for sletting av fullmakt i BPROF er klar for brevutsending
+        </Heading>
+      </Box.New>
+
+      <Form method="post" style={{ width: '20em' }}>
+        <VStack gap={'4'}>
+          <TextField
+            label="Behandling ID"
+            name="behandlingId"
+            onChange={(e) => {
+              setBehandlingId(e.target.value)
+            }}
+            value={behandlingId}
+          />
+
+          <Button variant="danger" type="submit" disabled={behandlingId === ''} loading={isSubmitting}>
+            Bekreft klar for brevutsending
+          </Button>
+        </VStack>
+      </Form>
+    </VStack>
+  )
+}
--- app/adhocbrev_fullmakter/adhoc-brev-fullmakt.server.ts
+++ app/adhocbrev_fullmakter/adhoc-brev-fullmakt.server.ts
@@ -25,3 +25,27 @@ export async function opprettAdHocBrevSlettFullmaktBprofBehandling(
 type StartBatchResponse = {
   behandlingId: number
 }
+
+export async function bekreftAdHocBrevSlettFullmaktBprofBrevUtsending(
+  accessToken: string,
+  behandlingId: string,
+): Promise<StartBatchResponse> {
+  const response = await fetch(
+    `${env.penUrl}/api/behandling/adhocBrevSlettFullmaktBprof/${behandlingId}/bekreftKlarForBrevUtsending`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+        'X-Request-ID': crypto.randomUUID(),
+      },
+    },
+  )
+
+  if (response.ok) {
+    return (await response.json()) as StartBatchResponse
+  } else {
+    console.log()
+    throw new Error()
+  }
+}
