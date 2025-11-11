dc3b6b2e427643bac6f5376ce351014e4161dfd7
Morten Normann <morten.normann@nav.no>
2025-11-11 09:37:36 +0100

    Bruk requestParam for opptjeningsår og godskrivingsår

--- app/opptjening/arlig/siste.omsorg.godskrivingsaar.server.ts
+++ app/opptjening/arlig/siste.omsorg.godskrivingsaar.server.ts
@@ -1,21 +1,24 @@
 import { data } from 'react-router'
 import { env } from '~/services/env.server'
 
-export async function oppdaterSisteOmsorgGodskrivingsaar(accessToken: string, opptjeningsar: number): Promise<string> {
-  const response = await fetch(`${env.penUrl}/api/opptjening/omsorggodskrivingsaar/oppdater`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      'Content-Type': 'application/json',
-      'X-Request-ID': crypto.randomUUID(),
+export async function oppdaterSisteOmsorgGodskrivingsaar(
+  accessToken: string,
+  godskrivingsaar: number,
+): Promise<string> {
+  const response = await fetch(
+    `${env.penUrl}/api/opptjening/omsorggodskrivingsaar/oppdater?godskrivingsaar=${godskrivingsaar}`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+        'X-Request-ID': crypto.randomUUID(),
+      },
     },
-    body: JSON.stringify({
-      opptjeningsar: opptjeningsar,
-    }),
-  })
+  )
 
   if (response.ok) {
-    return `Siste omsorg godskrivår er endret til ${opptjeningsar}`
+    return `Siste omsorg godskrivår er endret til ${godskrivingsaar}`
   } else {
     const text = await response.text().catch(() => '')
     throw data(`Feil ved start av oppdater siste omsorg godskrivår. Feil var\n${text}`, {
