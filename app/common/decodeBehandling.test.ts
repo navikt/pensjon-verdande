9f7313ad4d540c414e3cb5a0e278ecf328ddd4d4
Petter Walbø Johnsgård <1415747+walbo@users.noreply.github.com>
2026-02-04 16:18:00 +0100

    Fiks unit test

--- app/common/decodeBehandling.test.ts
+++ app/common/decodeBehandling.test.ts
@@ -2,13 +2,13 @@ import { expect, test } from 'vitest'
 import { decodeBehandling } from '~/common/decodeBehandling'
 
 test('deler opp på store bokstaver', () => {
-  expect(decodeBehandling('EtLangNavnBehandling')).toBe('Et Lang Navn')
+  expect(decodeBehandling('EtLangNavnBehandling')).toBe('Et lang navn')
 })
 
 test('beholder forkortelser samlet', () => {
-  expect(decodeBehandling('EtLangNavnAFPBehandling')).toBe('Et Lang Navn AFP')
+  expect(decodeBehandling('EtLangNavnAFPBehandling')).toBe('Et lang navn AFP')
 })
 
 test('støtter æøå', () => {
-  expect(decodeBehandling('HentLøpendeUføretrygdBehandling')).toBe('Hent Løpende Uføretrygd')
+  expect(decodeBehandling('HentLøpendeUføretrygdBehandling')).toBe('Hent løpende uføretrygd')
 })
