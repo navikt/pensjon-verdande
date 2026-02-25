0dc8c3eb191c5dbd538a9209713e048344cb2396
Yun Huang <yun.huang@nav.no>
2026-02-25 12:46:24 +0100

    Fiks feil

--- app/vedlikehold/feil-registrer-krav.tsx
+++ app/vedlikehold/feil-registrer-krav.tsx
@@ -2,10 +2,10 @@ import { Button, Heading, HStack, Textarea, VStack } from '@navikt/ds-react'
 import { useState } from 'react'
 import { Outlet, redirect, useFetcher } from 'react-router'
 import { apiPost } from '~/services/api.server'
-import type { Route } from './+types/laas-opp-sak'
+import type { Route } from './+types/feil-registrer-krav'
 
 export function meta(): Route.MetaDescriptors {
-  return [{ title: 'Feil registrer vedtak | Verdande' }]
+  return [{ title: 'Feil registrer krav | Verdande' }]
 }
 
 export const action = async ({ request }: Route.ActionArgs) => {
@@ -13,16 +13,18 @@ export const action = async ({ request }: Route.ActionArgs) => {
 
   const sakIderRaw = formData.get('sakIder')
 
-  if (typeof sakIderRaw === 'string') {
-    const sakIder = konverterTilListe(sakIderRaw)
-
-    const response = await apiPost<{ behandlingId: number }>('/api/feilregistrerkrav', { sakIder }, request)
-
-    if (!response) {
-      throw new Error('Opprettelse av feilregistrer krav returnerte ingen respons')
-    }
-    return redirect(`/behandling/${response.behandlingId}`)
+  if (typeof sakIderRaw !== 'string') {
+    throw new Error('Påkrevd felt "sakIder" mangler eller har feil format')
   }
+
+  const sakIder = konverterTilListe(sakIderRaw)
+
+  const response = await apiPost<{ behandlingId: number }>('/api/feilregistrerkrav', { sakIder }, request)
+
+  if (!response) {
+    throw new Error('Opprettelse av feilregistrer krav returnerte ingen respons')
+  }
+  return redirect(`/behandling/${response.behandlingId}`)
 }
 
 function konverterTilListe(sakIderText: string): string[] {
@@ -32,19 +34,20 @@ function konverterTilListe(sakIderText: string): string[] {
     .filter((id) => id !== '')
 }
 
-export default function LaasOppSakPage() {
+export default function FeilRegistrerKravPage() {
   const fetcher = useFetcher()
   const [sakIderText, setSakIderText] = useState<string>('')
   return (
     <VStack gap="space-20">
-      <Heading size="large">Lås opp sak</Heading>
+      <Heading size="large">Feilregistrer krav</Heading>
       <fetcher.Form method="post">
         <HStack gap="space-8" align="end">
           <Textarea
-            label={'Saker som skal ekskluderes'}
+            label={'Feil registrerte saker (en per linje)'}
             onChange={(e) => setSakIderText(e.target.value)}
             value={sakIderText}
             name="sakIder"
+            minRows={10}
           />
           <Button type="submit" name="action">
             Feil registrer saker
