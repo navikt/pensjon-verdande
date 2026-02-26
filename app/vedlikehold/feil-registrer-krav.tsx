cf62ddec4865a34e3716562f1dcadcfec9ad29f3
Yun Huang <yun.huang@nav.no>
2026-02-26 10:55:27 +0100

    legge til mulighet for å velge behandlingstype

--- app/vedlikehold/feil-registrer-krav.tsx
+++ app/vedlikehold/feil-registrer-krav.tsx
@@ -1,4 +1,4 @@
-import { Button, Heading, HStack, Textarea, VStack } from '@navikt/ds-react'
+import { Button, Heading, HStack, Select, Textarea, VStack } from '@navikt/ds-react'
 import { useState } from 'react'
 import { redirect, useFetcher } from 'react-router'
 import { apiPost } from '~/services/api.server'
@@ -8,10 +8,17 @@ export function meta(): Route.MetaDescriptors {
   return [{ title: 'Feil registrer krav | Verdande' }]
 }
 
+export const BEHANDLING_TYPE = {
+  GYLDIGGJOERING: 'GyldiggjørAlder',
+} as const
+
+export type Behandlingstype = (typeof BEHANDLING_TYPE)[keyof typeof BEHANDLING_TYPE]
+
 export const action = async ({ request }: Route.ActionArgs) => {
   const formData = await request.formData()
 
   const sakIderRaw = formData.get('sakIder')
+  const behandlingstypeRaw = formData.get('behandlingstype')
 
   if (typeof sakIderRaw !== 'string') {
     throw new Error('Påkrevd felt "sakIder" mangler eller har feil format')
@@ -19,7 +26,17 @@ export const action = async ({ request }: Route.ActionArgs) => {
 
   const sakIder = konverterTilListe(sakIderRaw)
 
-  const response = await apiPost<{ behandlingId: number }>('/api/feilregistrerkrav', { sakIder }, request)
+  const behandlingstype: Behandlingstype | null =
+    typeof behandlingstypeRaw === 'string' &&
+    Object.values(BEHANDLING_TYPE).includes(behandlingstypeRaw as Behandlingstype)
+      ? (behandlingstypeRaw as Behandlingstype)
+      : null
+
+  const response = await apiPost<{ behandlingId: number }>(
+    '/api/feilregistrerkrav',
+    { sakIder, behandlingstype },
+    request,
+  )
 
   if (!response) {
     throw new Error('Opprettelse av feilregistrer krav returnerte ingen respons')
@@ -41,18 +58,35 @@ export default function FeilRegistrerKravPage() {
     <VStack gap="space-20">
       <Heading size="large">Feilregistrer krav</Heading>
       <fetcher.Form method="post">
-        <HStack gap="space-8" align="end">
-          <Textarea
-            label={'Feil registrerte saker (en per linje)'}
-            onChange={(e) => setSakIderText(e.target.value)}
-            value={sakIderText}
-            name="sakIder"
-            minRows={10}
-          />
-          <Button type="submit" name="action">
-            Feil registrer saker
-          </Button>
-        </HStack>
+        <VStack gap="space-16">
+          <HStack gap="space-8" align="end">
+            <Textarea
+              label={'Feil registrerte saker (en per linje)'}
+              onChange={(e) => setSakIderText(e.target.value)}
+              value={sakIderText}
+              name="sakIder"
+              minRows={10}
+            />
+          </HStack>
+          <HStack gap="space-8" align="end">
+            <Select
+              label="Velg behandlingen som opprettet feil krav (valgfritt)"
+              name="behandlingstype"
+              defaultValue=""
+            >
+              <option value="" disabled>
+                Velg behandlingstype
+              </option>
+
+              <option value={BEHANDLING_TYPE.GYLDIGGJOERING}>{BEHANDLING_TYPE.GYLDIGGJOERING.valueOf()}</option>
+            </Select>
+          </HStack>
+          <HStack>
+            <Button type="submit" name="action">
+              Feil registrer saker
+            </Button>
+          </HStack>
+        </VStack>
       </fetcher.Form>
     </VStack>
   )
