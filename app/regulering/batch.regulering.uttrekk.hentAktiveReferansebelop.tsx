4efa4af61250492c04881e78f66c7012973d3b05
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2026-04-14 15:46:12 +0200

    Regulering: fikser route

--- app/regulering/batch.regulering.uttrekk.hentAktiveReferansebelop.tsx
+++ app/regulering/batch.regulering.uttrekk.hentAktiveReferansebelop.tsx
@@ -1,6 +1,6 @@
 import type { ReguleringAktiveReferansebelop } from '~/regulering/regulering.types'
 import { apiGet } from '~/services/api.server'
-import type { Route } from './+types/batch.regulering.orkestrering.hentAktiveReferansebelop'
+import type { Route } from './+types/batch.regulering.uttrekk.hentAktiveReferansebelop'
 
 export const loader = async ({ request }: Route.LoaderArgs) => {
   return await apiGet<ReguleringAktiveReferansebelop>('/api/vedtak/regulering/referansebelop', request)
