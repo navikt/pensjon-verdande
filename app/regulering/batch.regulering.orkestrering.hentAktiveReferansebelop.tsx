532b0e5633ad32e32fe9ddd055d7da73340bf35e
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2026-04-14 10:08:53 +0200

    Regulering: henter aktive referansebeløp fra Pen og viser disse på regulerings-side

---
+++ app/regulering/batch.regulering.orkestrering.hentAktiveReferansebelop.tsx
@@ -0,0 +1,7 @@
+import type { ReguleringAktiveReferansebelop } from '~/regulering/regulering.types'
+import { apiGet } from '~/services/api.server'
+import type { Route } from './+types/batch.regulering.orkestrering.hentAktiveReferansebelop'
+
+export const loader = async ({ request }: Route.LoaderArgs) => {
+  return await apiGet<ReguleringAktiveReferansebelop>('/api/vedtak/regulering/referansebelop', request)
+}
--- app/regulering/batch.regulering.uttrekk.tsx
+++ app/regulering/batch.regulering.uttrekk.tsx
@@ -2,22 +2,25 @@ import 'chart.js/auto'
 import {
   Alert,
   BodyLong,
+  BodyShort,
   Button,
   DatePicker,
+  Heading,
   HStack,
   Loader,
   Modal,
   ProgressBar,
   ReadMore,
+  Table,
   VStack,
 } from '@navikt/ds-react'
 import { format } from 'date-fns'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link, useFetcher, useOutletContext } from 'react-router'
 import { formatIsoDate, formatIsoTimestamp } from '~/common/date'
 import { useRevalidateOnInterval } from '~/common/useRevalidateOnInterval'
 import { Entry } from '~/components/entry/Entry'
-import type { ReguleringDetaljer } from '~/regulering/regulering.types'
+import type { ReguleringAktiveReferansebelop, ReguleringDetaljer } from '~/regulering/regulering.types'
 import { Behandlingstatus } from '~/types'
 
 export default function Uttrekk() {
@@ -25,7 +28,7 @@ export default function Uttrekk() {
 
   useRevalidateOnInterval({
     enabled: true,
-    interval: uttrekk?.status === Behandlingstatus.UNDER_BEHANDLING ? 500 : 1500,
+    interval: uttrekk?.status === Behandlingstatus.UNDER_BEHANDLING ? 500 : 6000,
   })
 
   const [isStartUttrekkOpen, setIsStartUttrekkOpen] = useState(false)
@@ -108,6 +111,11 @@ export default function Uttrekk() {
         {uttrekk?.status === Behandlingstatus.FULLFORT && (
           <Link to="/batch/regulering/orkestrering">Gå til Orkestrering</Link>
         )}
+      </HStack>
+      <HStack>
+        <VStack gap="space-20">
+          <ReferansebelopTabell />
+        </VStack>
       </HStack>
       <StartUttrekkModal isOpen={isStartUttrekkOpen} onClose={() => setIsStartUttrekkOpen(false)} />
       <OppdaterUttrekkModal isOpen={isOppdaterUttrekkOpen} onClose={() => setIsOppdaterUttrekkOpen(false)} />
@@ -195,5 +203,66 @@ function OppdaterUttrekkModal({ isOpen, onClose }: { isOpen: boolean; onClose: (
         </Button>
       </Modal.Footer>
     </Modal>
+  )
+}
+
+export function ReferansebelopTabell() {
+  const fetcher = useFetcher()
+
+  // On Mount
+  useEffect(() => {
+    if (fetcher.data === undefined && fetcher.state === 'idle') {
+      fetcher.load('hentAktiveReferansebelop')
+    }
+  }, [fetcher])
+
+  const aktiveReferansebelopWrapper = fetcher.data as ReguleringAktiveReferansebelop | undefined
+
+  if (aktiveReferansebelopWrapper === undefined && fetcher.state === 'loading') {
+    return <Loader size="small" title="Laster referansebelop…" />
+  }
+
+  if (aktiveReferansebelopWrapper === undefined) {
+    return null
+  }
+
+  const { referansebelop } = aktiveReferansebelopWrapper
+
+  return (
+    <VStack gap="space-20">
+      <Heading level="2" size="medium">
+        Referansebeløp
+      </Heading>
+      <HStack style={{ marginLeft: 'auto' }} gap="space-12">
+        {referansebelop.length > 0 && (
+          <Table size="small">
+            <BodyShort as="caption" visuallyHidden>
+              Referansebeløp
+            </BodyShort>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Årskull</Table.HeaderCell>
+                <Table.HeaderCell align="right">Beløp</Table.HeaderCell>
+                <Table.HeaderCell>Fra-og-med</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {referansebelop.map((belop) => (
+                <Table.Row key={belop.arskull + belop.referansebelop}>
+                  <Table.DataCell>{belop.arskull}</Table.DataCell>
+                  <Table.DataCell align="right">{belop.referansebelop}</Table.DataCell>
+                  <Table.DataCell>{belop.fomDato}</Table.DataCell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        )}
+        {referansebelop.length === 0 && (
+          <Alert variant="warning" inline>
+            Ingen aktive referansebeløp funnet i Pen
+          </Alert>
+        )}
+      </HStack>
+    </VStack>
   )
 }
