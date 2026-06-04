5d8d7e9667993ad83350291a1621ffba258f29f6
Petter Walbø Johnsgård <petter.walbo.johnsgard@nav.no>
2026-06-04 16:43:10 +0200

    Formater config fil

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,14 +1,14 @@
-import type { Config } from '@react-router/dev/config';
+import type { Config } from '@react-router/dev/config'
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
   future: {
-      v8_middleware: false,
-      v8_passThroughRequests: false,
-      v8_viteEnvironmentApi: false,
-      v8_splitRouteModules: false,
-      v8_trailingSlashAwareDataRequests: false,
+    v8_middleware: false,
+    v8_passThroughRequests: false,
+    v8_viteEnvironmentApi: false,
+    v8_splitRouteModules: false,
+    v8_trailingSlashAwareDataRequests: false,
   },
-} satisfies Config;
+} satisfies Config
